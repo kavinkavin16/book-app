d7fbf3c5d09b7eb947299fc2e82dbae3e1fecb29
kavinkavin16 <kavinnares@gmail.com>
2021-05-26 23:34:31 +0530

    changed Book.sql

--- Book.sql
+++ Book.sql
@@ -1,3 +1,3 @@
 create table book_2595(id number,name varchar(200),publishedDate date);
 insert into book_2595 values(1,"Java- The Helloworld",'12-May-2021');
-select * from cars;
+select * from book_2595;
